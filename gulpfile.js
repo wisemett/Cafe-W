59fd2688596fbcb0b987f70dbf4dad73047cc003
Baek2back <dumbuk12@naver.com>
2020-11-06 21:42:35 +0900

    feat: Add gulpfile.js to Automate file include

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const fileinclude = require('gulp-file-include');
+const gulp = require('gulp');
+
+gulp.task('fileinclude', function () {
+  return gulp
+    .src(['index.html'])
+    .pipe(
+      fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+      })
+    )
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', gulp.series('fileinclude'));
